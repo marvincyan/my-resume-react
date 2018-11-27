c959327031ceb3ed197cb07f607e4d9db0554304
Marvin <marvincyan@gmail.com>
2018-11-27 10:02:08 -0800

    Updates

---
+++ precache-manifest.a6ea4e3e29bf29d21e4438f30bd7e3d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15af348bc10a1a8fbc12",
+    "url": "/my-resume-react/static/js/runtime~main.15af348b.js"
+  },
+  {
+    "revision": "37981bb7d53153d6011b",
+    "url": "/my-resume-react/static/js/main.37981bb7.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/my-resume-react/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "37981bb7d53153d6011b",
+    "url": "/my-resume-react/static/css/main.6c5cb9d3.chunk.css"
+  },
+  {
+    "revision": "bc75642797ad2036049a939d389e18d6",
+    "url": "/my-resume-react/index.html"
+  }
+];
